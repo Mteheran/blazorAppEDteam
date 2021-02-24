1c9aa57d75b36f6b0f1aa0d2e529207a203a65b9
Miguel Teheran <miguelteheran@gmail.com>
2021-02-24 18:01:40 -0500

    adding dependency injection

---
+++ Services/WeatherForecastService.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static blazorApp.Pages.FetchData;
+
+namespace blazorApp.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        public HttpClient Http {get;set;}
+
+        public WeatherForecastService(HttpClient http)
+        {
+            Http = http;
+        }
+
+        public async Task<WeatherForecast[]> GetWeatherForecasts()
+        {
+            return await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+    }
+
+    public interface IWeatherForecastService
+    {
+        Task<WeatherForecast[]> GetWeatherForecasts();
+    }
+}
