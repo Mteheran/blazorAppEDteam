8f8f25a889610d85a6a147080e854884cbc34017
Miguel Teheran <miguelteheran@gmail.com>
2021-02-27 21:50:46 -0500

    adding razor library

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using blazorApp.Services;
+using Blazored.Toast;
 
 namespace blazorApp
 {
@@ -19,6 +20,8 @@ namespace blazorApp
             builder.RootComponents.Add<App>("#app");
 
             builder.Logging.SetMinimumLevel(LogLevel.Debug);
+
+            builder.Services.AddBlazoredToast();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
