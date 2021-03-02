b0bf2f151c8896e83ac3f8484e8be1e2c9a34d79
Miguel Teheran <miguelteheran@gmail.com>
2021-03-01 21:07:14 -0500

    adding validate and submit

--- BlazorApp/Models/ToDoTask.cs
+++ BlazorApp/Models/ToDoTask.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace blazorApp.Models
     public class ToDoTask
     {
         public Guid TaskId {get;set;}
+        [Required(ErrorMessage="Task is required")]
+        [StringLength(20)]
         public string Description {get;set;}
         public TaskState TaskState {get;set;}
         public DateTime DateCreated {get;set;} = DateTime.Now;
