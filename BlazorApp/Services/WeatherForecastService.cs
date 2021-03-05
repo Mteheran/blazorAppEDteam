b3121726fc0ef95fee2df449bb1af9216d722ef2
Miguel Teheran <miguelteheran@gmail.com>
2021-03-04 23:40:14 -0500

    adding toas erro rmessage

--- BlazorApp/Services/WeatherForecastService.cs
+++ BlazorApp/Services/WeatherForecastService.cs
@@ -4,22 +4,39 @@ using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Blazored.Toast.Services;
+using Microsoft.Extensions.Logging;
 using static blazorApp.Pages.FetchData;
 
 namespace blazorApp.Services
 {
     public class WeatherForecastService : IWeatherForecastService
     {
-        public HttpClient Http {get;set;}
+        private HttpClient Http {get;set;}
+        private ILogger<WeatherForecastService> Logger;
+        private IToastService ToastService;
 
-        public WeatherForecastService(HttpClient http)
+        public WeatherForecastService(HttpClient http, ILogger<WeatherForecastService> logger, IToastService toastService)
         {
             Http = http;
+            Logger = logger;
+            ToastService = toastService;
         }
 
         public async Task<WeatherForecast[]> GetWeatherForecasts()
         {
-            return await Http.GetFromJsonAsync<WeatherForecast[]>("https://localhost:5005/weatherforecast");
+            List<WeatherForecast> listResult = new List<WeatherForecast>();
+            try
+            {
+                listResult = (await Http.GetFromJsonAsync<WeatherForecast[]>("https://localhost:5005/weatherforecast")).ToList();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+                ToastService.ShowError("Error getting data");
+            }
+
+            return listResult.ToArray();
         }
     }
 
